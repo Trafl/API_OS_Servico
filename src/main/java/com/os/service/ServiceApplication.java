ef212fcab86d3810a98e3a4520fc9ef6ae0da0cd
P.Ivo <pivolluz@gmail.com>
2024-08-01 16:45:22 -0300

    Configurado as variaveis de ambiente e modificações no docker-compose

--- src/main/java/com/os/service/ServiceApplication.java
+++ src/main/java/com/os/service/ServiceApplication.java
@@ -5,13 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@Log4j2
 public class ServiceApplication {
-
 
     public static void main(String[] args) {
         SpringApplication.run(ServiceApplication.class, args);
-        log.info("Acesse a documentação no caminho http://localhost:8080/swagger-ui/index.html");
     }
 
 
--- src/main/java/com/os/service/core/swagger/OpenApiConfig.java
+++ src/main/java/com/os/service/core/swagger/OpenApiConfig.java
@@ -5,12 +5,19 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.media.Schema;
 import io.swagger.v3.oas.models.media.StringSchema;
+import jakarta.annotation.PostConstruct;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.ProblemDetail;
 
 @Configuration
+@Log4j2
 public class OpenApiConfig {
+
+    @Value("${API_OS_PORT}")
+    private String port;
 
     @Bean
     OpenAPI openAPI() {
@@ -28,5 +35,10 @@ public class OpenApiConfig {
                                 .addProperty("detail", new StringSchema().example("Order id 5 was not found"))
                                 .addProperty("timestamp", new StringSchema().example("2024-08-16sT19:32:54.253417400Z"))
                         ));
+    }
+
+    @PostConstruct
+    public void swaggerLog(){
+        log.info("Acesse a documentação no caminho http://localhost:"+ port + "/swagger-ui/index.html");
     }
 }
